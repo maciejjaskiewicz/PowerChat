010e8ee10b7168f1519ccded860ebd90186ca9f6
Maciej Jaśkiewicz <17217929+maciejjaskiewicz@users.noreply.github.com>
2019-11-02 12:00:56 +0100

    Project clean up, references

---
+++ src/PowerChat.API/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PowerChat.API.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return await Task.FromResult(Json(new
+            {
+                a = "a",
+                b = "b"
+            }));
+        }
+    }
+}
